e71d5680836e72fcf0317cd6870af4a986f097b3
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-05 16:05:32 +0530

    Added: Part time logic

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -5,18 +5,23 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Problem");
 		
-		final int FULL_TIME = 1;
+		final int FULL_TIME = 2;
+		final int PART_TIME = 1;
 		final int WAGE_PER_HR = 20;
 		int FullDayHours = 0;
-	    int empCheck = (int) (Math.random() * 100) % 2;
+	    int empCheck = (int) (Math.random() * 100) % 3;
 	    if (empCheck == FULL_TIME)
 	    {
-	        System.out.println("Employee is Present");
-	        FullDayHours = 8;
-	    } else
-	    {
-	        System.out.println("Employee is Absent");
-	    }
+            System.out.println("Employee is Present Full time");
+            FullDayHours = 16;
+        } else if (empCheck == PART_TIME)
+        {
+            System.out.println("Employee is Present Part time");
+            FullDayHours = 8;
+        } else
+        {
+            System.out.println("Employee is Absent");
+        }
 	    int wage = FullDayHours * WAGE_PER_HR;
 	    System.out.println("Employee Daily Wage is " + wage);
 	}
