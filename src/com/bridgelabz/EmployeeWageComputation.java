ad272f1d300421dd090249e48370665ac1e9bb53
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-02-05 15:48:28 +0530

    Added: Employee daily wage logic

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -6,14 +6,19 @@ public class EmployeeWageComputation {
 		System.out.println("Welcome to Employee Wage Computation Problem");
 		
 		final int FULL_TIME = 1;
+		final int WAGE_PER_HR = 20;
+		int FullDayHours = 0;
 	    int empCheck = (int) (Math.random() * 100) % 2;
 	    if (empCheck == FULL_TIME)
 	    {
 	        System.out.println("Employee is Present");
+	        FullDayHours = 8;
 	    } else
 	    {
 	        System.out.println("Employee is Absent");
 	    }
+	    int wage = FullDayHours * WAGE_PER_HR;
+	    System.out.println("Employee Daily Wage is " + wage);
 	}
 }
 
